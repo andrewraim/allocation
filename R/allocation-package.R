34aea2c8ffd7d92cc60f5011e20abd83813c3db8
Andrew Raim <andrew.raim@census.gov>
2025-08-13 11:20:26 -0400

    Some intermediate updates

--- R/allocation-package.R
+++ R/allocation-package.R
@@ -3,9 +3,7 @@
 #' 
 #' @description 
 #' Algorithms III and IV from Wright (2017), and classical (unconstrained)
-#' Neyman allocation. Algorithm III finds the optimial allocation for a
-#' given total sample size \code{n}. Algorithm IV samples until the overall
-#' variance is smaller than a given \code{v0}.
+#' Neyman allocation.
 #' 
 #' @param n Target sample size for Algorithm III (integer)
 #' @param v0 Target variance for Algorithm IV (numeric)
@@ -19,17 +17,20 @@
 #' allocation method.
 #' 
 #' @details
+#' Algorithm III finds the optimial allocation for a given total sample size
+#' `n`. Algorithm IV allocates units until the overall variance is smaller than
+#' a given `v0`.
+#' 
 #' Global options for the package (with defaults) are:
-#' \itemize{
-#' \item \code{options(allocation.prec.bits = 256)} Number of bits of
-#'     precision to use with \code{mpfr} objects in internal calculations.
-#' \item \code{options(allocation.print.decimals = 4)} Number of decimals
-#'     to display in output.
-#' \item \code{options(allocation.algIV.tol = 1e-10)} A small positive
-#'     number for use in algIV; if all strata have \code{V_str <= tol},
-#'     no more allocation is possible, even if target value \code{v0} has
-#'     not yet been attained.
-#' }
+#' 
+#' - `options(allocation.prec.bits = 256)` Number of bits of
+#'   precision to use with `mpfr` objects in internal calculations.
+#' - `options(allocation.print.decimals = 4)` Number of decimals
+#'   to display in output.
+#' - `options(allocation.algIV.tol = 1e-10)` A small positive
+#'   number for use in Algorithm IV; if all strata have `V_str <= tol`,
+#'   no more allocation is possible, even if target value `v0` has
+#'   not yet been attained.
 #'
 #' @references
 #' Tommy Wright (2012). The Equivalence of Neyman Optimum Allocation for
